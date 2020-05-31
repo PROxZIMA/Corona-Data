7546d60f1ecbdfbcce993cdba5fd2f178a328c43
= <=>
2020-05-31 23:00:21 +0530

    Great changes with optimization
    table extraction optimized and cleaned

--- Corona-Data.py
+++ Corona-Data.py
@@ -5,64 +5,67 @@ import numpy as np
 import pandas as pd
 
 
-#A function which returns data as {'cases':[1, 2, 3], 'States':['Goa', 'Kerala', 'other stater']}
+
 def tabledata(tablehtml):
+    """
+    A function which returns data as {'cases':[1, 2, 3], 'States':['Goa', 'Kerala', 'other stater']}
+    It takes html format of a table and returns a well-defined dictionery
+    """
     data = {}
+
     #Finds column heads
     table_heads = tablehtml.find('thead').find('tr').findAll('th')
-    row_h = [table_head.find('strong').text.strip() for table_head in table_heads]
     #Create a dictionary with keys as Column head and values as empty list
-    for i in range(5): data[row_h[i]] = []
+    for table_head in table_heads:
+        data[table_head.text.strip()] = []
 
     #Finds data under a specific column
-    table_bodys = tablehtml.find('tbody').findAll('tr')
+    table_bodies = tablehtml.find('tbody').findAll('tr')
     count = 0
-    for table_body in table_bodys:
-        table_body_datas = table_body.findAll('td')
-        #Row as a list
-        row_b = [table_body_data.text.strip() for table_body_data in table_body_datas]
+    for table_body in table_bodies:
+
+        #Limits the total reach of the table_body
+        count += 1
+        if count == 38: break
 
         #Append each data of a row to individual key
-        #If initial sict is like {'cases':[], 'States':[]}
+        #If initial dict is like {'cases':[], 'States':[]}
         #The this loop gives {'cases':[1], 'States':['Goa']} if row is [1, 'Goa']
         #Then in next loop gives {'cases':[1, 2], 'States':['Goa', 'Kerala']} if next row is [2, 'Kerala']
-        for (i, j), k in zip(data.items(), range(5)): j = j.append(row_b[k])
+        for (i, j), table_body_data in zip(data.items(), table_body.findAll('td')):
+            try:
+                j.append(int(table_body_data.text.strip()))
+            except ValueError:
+                j.append(table_body_data.text.strip())
 
-        #This prevents breaking of Panadas
-        count += 1
-        if count == 32: break
-    
-    #Converts String numbers to integer datatype
-    data['Total Cases'] = data.pop('Total Confirmed cases (Including 111 foreign Nationals)')
-    data['Total Cases'] = list(map(int, data['Total Cases']))
-    data['Death'] = list(map(int, data['Death']))
-    data['Cured/Discharged/Migrated'] = list(map(int, data['Cured/Discharged/Migrated']))
-    
     return data
 
 
-def graph(tabledata):
-    #Creates a dataframe. Here tabledata is a well defined dictionery
-    dftable = pd.DataFrame(tabledata)
+def graph(dftable):
+    """
+    Displays a graph using the dictionary's dataframe
+    """
+    #Removes last 2 rows from the data frame for plotting purpose
+    dftable = dftable.drop(dftable.index[[-2, -1]])
     #Sorts whole dataframe in ascending order
-    dftable = dftable.sort_values('Total Cases')
-    #Creates a wellspaced array
-    x = np.linspace(1, len(tabledata['S. No.']), len(tabledata['S. No.']))
+    dftable = dftable.sort_values('Total Confirmed cases*')
+    #Creates a wellspaced array equal to rows in table
+    x = np.linspace(1, len(dftable.index), len(dftable.index))
 
     #Used for plotting multiple data in same frame
     ax = plt.subplot()
     #Creates bar according to the dataframe
-    bar_c = ax.bar(x-0.2, list(dftable['Cured/Discharged/Migrated']), width=0.2, color='g', label='Cured Cases')
-    bar_d = ax.bar(x, list(dftable['Death']), width=0.2, color='r', label='Deaths')
-    bar_t = ax.bar(x+0.2, list(dftable['Total Cases']), width=0.2, color='b', label='Total Cases')
-    
+    bar_c = ax.bar(x-0.2, dftable['Cured/Discharged/Migrated*'], width=0.2, color='g', label='Cured Cases')
+    bar_d = ax.bar(x, dftable['Deaths**'], width=0.2, color='r', label='Deaths')
+    bar_t = ax.bar(x+0.2, dftable['Total Confirmed cases*'], width=0.2, color='b', label='Total Confirmed cases')
+
     #Set the x ticks with list of ticks
     ax.set_xticks(x)
     #Set the labels to the ticks
-    ax.set_xticklabels(list(dftable['Name of State / UT']))
+    ax.set_xticklabels(dftable['Name of State / UT'])
 
     #Set the y ticks with list of ticks
-    ax.set_yticks(np.linspace(0, 10000, 21))
+    ax.set_yticks(np.linspace(0, 80000, 21))
 
     #Create a legend
     ax.legend()
@@ -80,7 +83,7 @@ def graph(tabledata):
             #Formatting and placing the height
             ax.annotate(f'{height}',
                         xy=(rect.get_x() + rect.get_width() / 2, height),
-                        fontsize='5.5',
+                        fontsize='5.6',
                         xytext=(0, 5),
                         textcoords="offset points",
                         ha='center', va='bottom', rotation='90')
@@ -107,19 +110,11 @@ if __name__ == '__main__':
     #Get dictionary output from 'table' data function
     table = tabledata(table_site)
 
-    #Creates a dataframe of the table
+    #Creates a dataframe. Here table is a well defined dictionery
     dftable = pd.DataFrame(table)
-
-    #Adds a row to the dataframe
-    dftable.loc[dftable.index[-1]+  1] = ['','Total number of confirmed cases',dftable['Cured/Discharged/Migrated'].astype(int).sum(),dftable['Death'].astype(int).sum(),dftable['Total Cases'].astype(int).sum()]
 
     #Prints dataframe without the indices
     print(dftable.to_string(index=False))
-    print('Active Cases:', dftable.iloc[-1]['Total Cases']-dftable.iloc[-1]['Cured/Discharged/Migrated']-dftable.iloc[-1]['Death'])
-    
+
     #Displays the Graph
-    graph(table)
-
-
-
-
+    graph(dftable)
